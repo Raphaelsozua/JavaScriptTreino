56045fe02457be2a13d873350d30c35c565a61af
raphael souza <raphabaru@gmail.com>
2023-09-11 16:32:00 -0300

    commitando

---
+++ String.js
@@ -0,0 +1,35 @@
+/*let altura = 5;
+
+let comprimento = 7;
+
+let forma = 'retangulo';
+
+let area;
+
+
+if (forma === 'retangulo'){
+    area = altura * comprimento;
+} else {
+    area = altura * comprimento /2;
+}
+console.log(area) */
+
+/* const forma = 'quadrada';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === "quadrada"){
+    area = altura * comprimento;
+}else{
+    area = altura * comprimento /2;
+}
+
+console.log(area)
+*/
+
+const usuario = true;
+const contapaga = false;
+
+
+
---
+++ teste.js
@@ -0,0 +1,16 @@
+const meuNumero = 3;
+
+const primeiroNumero = 10;
+const segundoNumero = 6;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+const pontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / pontoFlutuante;
+console.log(novaOperacao)
+
